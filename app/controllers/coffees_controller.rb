d2ea2d51c70676360dd53a44576d1c1acd3d9bfe
Matthew Thomas <mzthomas96@gmail.com>
2021-03-08 11:32:55 -0500

    Backend Skeleton Done

--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -1,2 +1,52 @@
 class CoffeesController < ApplicationController
+    def index
+        @coffees = Coffee.all
+        render json: @coffees
+    end
+
+    def show
+        @coffee = Coffee.find(params[:id])
+        render json: @coffee
+    end
+
+
+    def create
+        @coffee = Coffee.create(coffee_params)
+        render json: @coffee 
+    end
+
+
+    def update
+        @coffee = Coffee.find(params[:id])
+        @coffee.update(Coffees_params)
+        render json: @coffee
+    end
+
+
+    def destroy
+        @coffee = Coffee.find(params[:id])
+        @coffee.destroy
+        render json: @coffee
+    end
+
+
+    # def count
+    #     render json: {count: Coffee.count}
+    # end
+
+    # def sum
+    #     total = params[:arrayOfNums].sum
+    #     render json: {"here is your sum": total}
+    # end
+
+
+
+    private
+
+
+    def coffee_params
+        # params.require(:coffee).permit(:name, :image, :likes)
+        params.permit(:name)
+    end
+
 end
